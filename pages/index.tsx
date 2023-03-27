77b07b2b07ddb88953f0172162ce137cbf0d01f1
Alejandro <alejandrom.am33@gmail.com>
2023-03-27 01:29:32 -0600

    added next ui library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Button from "@nextui-org/react/esm/button";
 import { FC } from "react";
 
 const Home: FC = () => {
   return (
     <>
       <h1>Home</h1>
+      <Button color={"warning"}>ola</Button>
     </>
   );
 };
