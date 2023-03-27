61c548ee4eca83afabaa8b15944e4114ef5fdfa8
Alejandro <alejandrom.am33@gmail.com>
2023-03-27 03:27:31 -0600

    added grid of pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,45 @@
-import { Layout } from "../components/layouts";
-import Button from "@nextui-org/react/esm/button";
+import { GetStaticProps } from "next";
 import { FC } from "react";
 
-const Home: FC = () => {
+import { Grid } from "@nextui-org/react";
+
+import { Layout } from "../components/layouts";
+import { pokeApi } from "@/api";
+import { PokemonListResponse, SmallPokemon } from "@/interfaces";
+import { PokemonCard } from "@/components/pokemon";
+
+interface HomeProps {
+  pokemons: SmallPokemon[];
+}
+
+const Home: FC<HomeProps> = ({ pokemons }) => {
   return (
     <Layout title="Todos los Pokémons">
-      <h1>Home</h1>
-      <Button color={"warning"}>ola</Button>
+      <Grid.Container gap={2} justify="flex-start">
+        {pokemons.map((pokemon, index) => (
+          <PokemonCard key={index} pokemon={pokemon} />
+        ))}
+      </Grid.Container>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => {
+    const id: number = index + 1;
+    const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    return {
+      ...pokemon,
+      id,
+      img,
+    };
+  });
+
+  return {
+    props: { pokemons },
+  };
 };
 
 export default Home;
